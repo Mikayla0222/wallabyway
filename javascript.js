2d1250e46f47e44b623f5a49be7bc13939df2aad
Mikayla0222 <mikayla0222@gmail.com>
2018-06-29 14:01:56 -0400

    files added

---
+++ javascript.js
@@ -0,0 +1,12 @@
+/*
+function hello(){
+  alert("hellooo, this is my website");
+}
+*/
+
+
+function myFunction() {
+    var string = "Favorites Page";
+    var result = string.link("favorites.html");
+    document.getElementById("button").innerHTML = result;
+}
