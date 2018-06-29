89ea36b7cfe10336aa9af78bde1daeb630176484
Mikayla0222 <mikayla0222@gmail.com>
2018-06-29 15:45:43 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,3 +10,24 @@ function myFunction() {
     var result = string.link("favorites.html");
     document.getElementById("button").innerHTML = result;
 }
+
+function hide(id){
+    var x = document.getElementById(id);
+    x.style.display = "none";
+}
+
+function showandhidemovies() {
+    var x = document.getElementById("starwasr");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+
+    var y = document.getElementById("bttf");
+    if (y.style.display === "none") {
+        y.style.display = "block";
+    } else {
+        y.style.display = "none";
+    }
+}
